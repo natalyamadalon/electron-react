acfb11b9426114466e06f3b433c5121aeaf335a2
denerbatista <denerbatista@icloud.com>
2023-10-23 21:20:34 -0300

    feat: style home

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,4 +1,12 @@
-import { Flex, Input } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  Input,
+  Textarea,
+  background,
+} from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
 interface IProps {
@@ -16,7 +24,6 @@ const Home = ({ setLogged }: IProps) => {
 
   return (
     <>
-      <h1>Home</h1>
       <button onClick={logout}>Logout</button>
       <Flex
         border={"1px solid black"}
@@ -24,17 +31,91 @@ const Home = ({ setLogged }: IProps) => {
         height={"40rem"}
         flexDirection={"column"}
         alignItems={"center"}
+        gap={"1rem"}
+        justifyContent={"center"}
+        borderRadius={"2rem"}
+        margin={"auto"}
+        backgroundColor={"white"}
+        color={"black"}
+        boxShadow={"1rem 1rem 2rem 0rem"}
       >
-        <label>Token Bot:</label>
-        <Input
-          id="token-bot"
-          width={"80%"}
-          height={"1rem"}
-          padding={"0.5rem"}
-        />
+        <h1 id="title-h1">Programar Mensagem telegram</h1>
 
-        <label>Chat Id:</label>
-        <Input id="chat-id" width={"80%"} height={"1rem"} padding={"0.5rem"} />
+        <FormControl color={"black"} display={"flex"} flexDirection={"column"}>
+          <label>Token Bot:</label>
+          <Input
+            id="token-bot"
+            width={"48rem"}
+            height={"1rem"}
+            padding={"0.5rem"}
+            borderRadius={"0.5rem"}
+          />
+        </FormControl>
+
+        <FormControl color={"black"} display={"flex"} flexDirection={"column"}>
+          <label>Chat Id:</label>
+          <Input
+            id="chat-id"
+            width={"48rem"}
+            height={"1rem"}
+            padding={"0.5rem"}
+            borderRadius={"0.5rem"}
+          />
+        </FormControl>
+
+        <FormControl color={"black"} display={"flex"} flexDirection={"column"}>
+          <label>Mensagem:</label>
+          <Textarea
+            id="mensagem"
+            width={"48rem"}
+            height={"3rem"}
+            padding={"0.5rem"}
+            borderRadius={"0.5rem"}
+          ></Textarea>
+        </FormControl>
+
+        <Box
+          color={"black"}
+          borderStyle={"dotted"}
+          width={"80%"}
+          height={"6rem"}
+          padding={"0.5rem"}
+          borderRadius={"1.5rem"}
+          fontSize={"2rem"}
+          textAlign={"center"}
+          display={"flex"}
+          justifyContent={"center"}
+          flexDirection={"column"}
+        >
+          <text>Clique ou arraste para adicionar arquivos</text>
+        </Box>
+
+        <Box
+          color={"black"}
+          display={"flex"}
+          justifyContent={"flex-start"}
+          width={"80%"}
+        >
+          <span>Imagens selecionadas:</span>
+        </Box>
+
+        <Button
+          padding={"1.5rem 2.3rem"}
+          borderRadius={"1.3rem"}
+          fontSize={"1.3rem"}
+          fontWeight={"600"}
+          backgroundColor={"#00a2ff"}
+          color={"white"}
+          border={"none"}
+          cursor={"pointer"}
+          _hover={{
+            color: "#00a2ff",
+            border: "1px solid",
+            background: "white",
+          }}
+        >
+          Programar mensagem
+        </Button>
       </Flex>
     </>
   );
